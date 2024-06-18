150a0520b186f72741220afd3cfe1597c737cf2c
Adalberto <asouza@wayfair.com>
2024-06-18 00:05:22 -0400

    RecipeApp, browse screen and detail page

---
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.recipeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.example.recipeapp.ui.theme.RecipeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val navController = rememberNavController()
+            RecipeAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    RecipeApp(navHostController = navController)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/recipeapp/RecipeApp.kt
@@ -0,0 +1,32 @@
+package com.example.recipeapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun RecipeApp(navHostController: NavHostController) {
+    val recipeViewModel: MainViewModel = viewModel()
+    val viewstate by recipeViewModel.categoriesState
+
+    NavHost(navController = navHostController, startDestination = Screen.RecipeScreen.route) {
+        composable(route = Screen.RecipeScreen.route) {
+            RecipeScreen(
+                viewstate = viewstate,
+                navigateToDetail = {
+                    navHostController.currentBackStackEntry?.savedStateHandle?.set("category", it)
+                    navHostController.navigate(Screen.DetailScreen.route)
+                },
+            )
+        }
+        composable(route = Screen.DetailScreen.route) {
+            val category =
+                navHostController.previousBackStackEntry?.savedStateHandle?.get<Category>("category")
+                    ?: Category("", "", "", "")
+            CategoryDetailScreen(category = category)
+        }
+    }
+}
